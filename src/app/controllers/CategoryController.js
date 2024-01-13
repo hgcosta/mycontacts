9f9e5f533c0e5bfd81d76bf3b43b5024c574712a
hgcosta <hugocosta.devs@gmail.com>
2024-01-13 19:10:13 -0300

    feature: create routers from categories

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,32 @@
+const CategoriesRepository = require("../repositories/CategoriesRepository");
+
+class CategoryController {
+    async index(request, response) {
+        const categories = await CategoriesRepository.findAll();
+
+        response.json(categories);
+    }
+    async show(request, response) {
+        const { id } = request.params;
+
+        const category = await CategoriesRepository.findById(id);
+
+        if (!category) {
+            return response.status(404).json({ error: "Category not found" });
+        }
+
+        response.json(category);
+    }
+    async store(request, response) {
+        const { name } = request.body;
+
+        if (!name) {
+            return response.status(400).json({ error: "Name is required" });
+        }
+        const category = await CategoriesRepository.create({ name });
+
+        response.json(category);
+    }
+}
+
+module.exports = new CategoryController();
